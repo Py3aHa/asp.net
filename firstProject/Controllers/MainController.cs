85b87c5a60637424bda68972c69f6696f9b605b4
Py3aHa <51872352+Py3aHa@users.noreply.github.com>
2020-04-16 12:01:30 +0600

    Update MainController.cs

--- firstProject/Controllers/MainController.cs
+++ firstProject/Controllers/MainController.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace firstASP.Properties
+namespace Project.Controllers
 {
     public class MainController : Controller
     {
